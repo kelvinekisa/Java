fa0a411fe8a809a3c7d6a3f8268432f01457da5a
kelvinekisa <kelvinekisa8@gmail.com>
2024-04-03 17:25:25 +0300

    methods

--- Methods.java
+++ Methods.java
@@ -33,6 +33,22 @@ public class Methods {
             System.out.println("Access Denied - You are not old enough");
         }else {System.out.println("Access granted - you ae old enough");}
     }
+
+    //calculate volume of a cuboid
+    static void calculateVolume(int length, int width,int height ) {
+        int volume = length * width * height;
+        System.out.println("The volume of a box is equal to: " + volume);
+
+    }
+
+    //invoking a method with different parameters
+
+
+
+    static void printStudent(String name, double averagescore) {
+        System.out.println("Name: " + name);
+        System.out.println("Average Score: " + averagescore);
+    }
     public static void main(String[] args) {
         myMethod("Kelvin");
         Area(10, 30);
@@ -40,7 +56,11 @@ public class Methods {
         int total = cost(200, 05);
         System.out.println(total);
         checkage(12);
+        calculateVolume(12, 12, 12);
         
+
+        // calling  function with correct parametrs
+        printStudent("Kelvin Ekisa", 364);
         /*
          * A method is a sequence of statements that can be invoked or reffered to by it's name
          * Parameters a value used inside a method
@@ -54,6 +74,31 @@ public class Methods {
          * The VOID keyword used indicates that the method should not return a value
          * if you want the methpod to return a value you should use primitive datatypes such as int, char
          * and use the return keyword inside the method
+         * 
+         * when calling a function you should always provide the correct number and types or arguments that the function expects
+         * 
+         * 
+         * Not all methods expect any parameters; some are designed to operate without any provided data
+         */
+
+// Declaring methods
+        /*
+         * Access modifiers
+          non access modifiers
+
+          access modifiers define the  visibility of the method
+          public means there are no restrictions for invoking the method from anywhere
+
+          non acess modifiers provide information about the  behaviour of methods to JVM
+          the modifier static means that the method belongs to the class and it can be accessed without creating any object.
+          its called a static method
+
+          if a method is declared without the static modifier, i means that the method can e invoked only through or with an obkect or instance of this class. instance methods
+
+
+          method parameters
+          we define, the type, number, and order of the parameters
+          
          */
         
     }
